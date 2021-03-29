376a4efb73100042e34cf7acfd7df5b63b43d35b
Cosmotora <victoriavldm@gmail.com>
2021-03-29 18:17:05 +0300

    Date parsing now via mapping

--- parse_magnit.py
+++ parse_magnit.py
@@ -1,12 +1,9 @@
 import time
 from datetime import datetime
-import locale
 import requests
 from urllib.parse import urljoin
 import bs4
 import pymongo
-
-locale.setlocale(locale.LC_ALL, 'Russian_Russia.1251')  # нужно для перевода дат
 
 
 class MagnitParse:
@@ -38,17 +35,14 @@ class MagnitParse:
 
         def _make_date(txt: str, date_from: bool) -> datetime:
             txt = txt.strip().split()
+            date_map = {'янв': '01', 'фев': '02', 'мар': '03', 'апр': '04', 'мая': '05', 'июн': '06',
+                        'июл': '07', 'авг': '08', 'сен': '09', 'окт': '10', 'ноя': '11', 'дек': '12'}
             if len(txt) < 5:
                 date_from = True
             i = 1 if date_from else 4
-            for letter in ['ь', '', 'й']:
-                try:
-                    date_str = txt[i] + ' ' + txt[i + 1][:-1] + letter
-                    date = datetime.strptime(date_str, '%d %B').replace(year=datetime.now().year)
-                    return date
-                except ValueError:
-                    continue
-            raise AttributeError
+            date_str = txt[i] + ' ' + date_map.get(txt[i + 1][:3], '{0:0>2}'.format(datetime.now().month))
+            date = datetime.strptime(date_str, '%d %m').replace(year=datetime.now().year)
+            return date
 
         data_template = {
             "url": lambda a: urljoin(self.start_url, a.attrs.get("href", "/")),
